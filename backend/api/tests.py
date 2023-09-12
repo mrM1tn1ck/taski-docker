f310d9a6191e81e70c5bd49b59eab198ec8b4a29
mrm1tn1ck <mrjohniut@gmail.com>
2023-09-12 12:06:34 +0500

    Add Django tests

---
+++ backend/api/tests.py
@@ -0,0 +1,21 @@
+from http import HTTPStatus
+
+from api import models
+from django.test import Client, TestCase
+
+
+class TaskiAPITestCase(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_list_exist(self):
+        """Прверка доступности списка задач."""
+        response = self.guest_client.get('/api/taski/')
+        self.assertEqual(response.status_code, HTTPstatus.OK)
+
+    def test_task_creation(self):
+        """Проверка создания задачи"""
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPstatus.CREATED)
+        self.asserTrue(models.Task.objects.filter(title='Test').exist())
